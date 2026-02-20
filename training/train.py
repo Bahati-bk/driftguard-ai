239fbf0e4279528be8d0d65f09cd6e77b717712b
Bahati-bk <162678744+Bahati-bk@users.noreply.github.com>
2026-02-20 13:08:47 +0300

    Fixing runtime errors

--- training/train.py
+++ training/train.py
@@ -1,4 +1,8 @@
-# training/train.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
